a3d22cd80c8d37c8e402170fcf1e95499a0cb4e8
roberta <roberta.gardiman@qwentes.it>
2019-12-03 12:03:22 +0100

    refactor(bookingConditions): component booking conditions

---
+++ src/components/qw-book-condition/qw-book-condition.tsx
@@ -0,0 +1,61 @@
+import {Component, Host, h, State} from '@stencil/core';
+import {
+  QuoteCreateBody,
+  QuoteHelper,
+  QuoteModel,
+  QuoteService,
+  SessionLoaded$,
+  SessionService,
+} from '@qwentes/booking-state-manager';
+import {switchMap} from 'rxjs/operators';
+
+@Component({
+  tag: 'qw-book-condition',
+  styleUrl: 'qw-book-condition.css',
+  shadow: false
+})
+export class QwBookCondition {
+  @State() quote: QuoteModel;
+  @State() formQuote: QuoteCreateBody;
+
+  public componentDidLoad() {
+    this.formQuote = QuoteHelper.initObjectForCreateBody();
+    SessionService.getSession().subscribe();
+    SessionLoaded$
+      .pipe(switchMap(session => {
+        return QuoteService.getQuote(session.sessionId);
+      }))
+      .subscribe((quote: QuoteModel) => this.quote = quote);
+  }
+
+  render() {
+    return (
+      <Host>
+        <slot>
+          {this.quote
+          && <div class="qw-book__booking-conditions">
+            <h4>Booking & Sales Conditions</h4>
+            <div class="qw-book__booking-conditions__cancellation">
+              <h5>Cancellation Policy</h5>
+              <li>{QuoteHelper.getDefaultCancelConditionMessage(this.quote)}</li>
+            </div>
+            <div class="qw-book__booking-conditions__deposit">
+              <h5>Deposit</h5>
+              <li>{this.quote.depositConditions.text}</li>
+            </div>
+            {this.quote.taxes.excludedTaxes.totalAmount.text && <div class="qw-book__booking-conditions__excluded-taxes">
+              <h5>Taxes excluded in the room price</h5>
+              <li>{`${this.quote.taxes.excludedTaxes.computations[0].summary} (${this.quote.taxes.excludedTaxes.totalAmount.text})`}</li>
+            </div>}
+            {this.quote.taxes.onSiteTaxes.totalAmount.text && <div class="qw-book__booking-conditions__on-site-taxes">
+              <h5>Taxes added to the price of the room</h5>
+              {this.quote.taxes.onSiteTaxes.totalAmount.text &&
+              <li>{`${this.quote.taxes.onSiteTaxes.computations[0].summary} (${this.quote.taxes.onSiteTaxes.totalAmount.text})`}</li>}
+            </div>}
+          </div>}
+        </slot>
+      </Host>
+    );
+  }
+
+}
--- src/components/qw-book/qw-book.tsx
+++ src/components/qw-book/qw-book.tsx
@@ -125,26 +125,7 @@ export class QwBook {
                   </div>
                 </div>
                 {this.quote
-                && <div class="qw-book__booking-conditions">
-                  <h4>Booking & Sales Conditions</h4>
-                  <div class="qw-book__booking-conditions__cancellation">
-                    <h5>Cancellation Policy</h5>
-                    <li>{QuoteHelper.getDefaultCancelConditionMessage(this.quote)}</li>
-                  </div>
-                  <div class="qw-book__booking-conditions__deposit">
-                    <h5>Deposit</h5>
-                    <li>{this.quote.depositConditions.text}</li>
-                  </div>
-                  {this.quote.taxes.excludedTaxes.totalAmount.text && <div class="qw-book__booking-conditions__excluded-taxes">
-                    <h5>Taxes excluded in the room price</h5>
-                    <li>{`${this.quote.taxes.excludedTaxes.computations[0].summary} (${this.quote.taxes.excludedTaxes.totalAmount.text})`}</li>
-                  </div>}
-                  {this.quote.taxes.onSiteTaxes.totalAmount.text && <div class="qw-book__booking-conditions__on-site-taxes">
-                    <h5>Taxes added to the price of the room</h5>
-                    {this.quote.taxes.onSiteTaxes.totalAmount.text &&
-                    <li>{`${this.quote.taxes.onSiteTaxes.computations[0].summary} (${this.quote.taxes.onSiteTaxes.totalAmount.text})`}</li>}
-                  </div>}
-                </div>}
+                && <qw-book-condition></qw-book-condition>}
                 <div class="qw-book__confirmation">
                   <h4>Confirmation</h4>
                   <div class="qw-book__confirmation-checkbox">
