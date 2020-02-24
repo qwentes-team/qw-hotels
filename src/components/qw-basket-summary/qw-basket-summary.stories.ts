b471607f82d45fd1ab9c9bed33d8ca6608ae3f48
Matteo Barone <barmat90@gmail.com>
2020-02-24 16:46:42 +0100

    feat(basket-summary): stories

---
+++ src/components/qw-basket-summary/qw-basket-summary.stories.ts
@@ -0,0 +1,10 @@
+export default {
+  title: 'Basket/Basket summary',
+};
+
+export const base = () => `
+  <qw-basket-summary></qw-basket-summary>
+  <hr>
+  <h4>Use this room component to play and mutate the basket summary behaviour</h4>
+  <qw-room-detail qw-room-detail-id="98868"></qw-room-detail>
+`;
