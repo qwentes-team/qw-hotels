3b5459119bb9dddf94470a19454c036f07c937bc
Matteo Barone <barmat90@gmail.com>
2020-02-27 12:00:49 +0100

    style(offers): display grid

---
+++ src/components/qw-offers/qw-offers.stories.ts
@@ -0,0 +1,9 @@
+export default {
+  title: 'Offer/Offers',
+}
+
+export const base = () => `<qw-offers></qw-offers>`;
+
+export const maximumNumber = () => `<qw-offers qw-offers-max="4"></qw-offers>`;
+
+export const card = () => `<qw-offers qw-offers-type="card"></qw-offers>`;
