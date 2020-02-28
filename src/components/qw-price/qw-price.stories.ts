3a98a8d0299bb2b7d381782febb62c31d3133451
Matteo Barone <barmat90@gmail.com>
2020-02-28 15:09:25 +0100

    fix(price): story

---
+++ src/components/qw-price/qw-price.stories.ts
@@ -0,0 +1,11 @@
+export default {
+  title: 'Price'
+}
+
+export const base = () => `
+  <qw-price
+    qw-price-main-price="1200 €"
+    qw-price-crossed-price="999 €"
+    qw-price-caption="Total for 3 nights">
+  </qw-price>
+`;
