730753068e5355ef757b80247f6b077a360cf71f
Matteo Barone <barmat90@gmail.com>
2020-02-28 16:11:06 +0100

    feat(book-condition): story

---
+++ src/components/qw-book-condition/qw-book-condition.stories.ts
@@ -0,0 +1,9 @@
+export default {
+  title: 'Book/Book condition',
+};
+
+export const base = () => `
+  <qw-book-condition qw-book-condition-force-quote-call="true"></qw-book-condition>
+  <hr>
+  <h4>If empty, add some rooms to basket</h4>
+`;
