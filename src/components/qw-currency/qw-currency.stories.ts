4c70728fb24a3783216f217e31f999da80e7f8e6
Matteo Barone <barmat90@gmail.com>
2020-02-28 16:23:55 +0100

    feat(currency): stories

---
+++ src/components/qw-currency/qw-currency.stories.ts
@@ -0,0 +1,13 @@
+export default {
+  title: 'Currency',
+}
+
+export const base = () => `
+  <qw-currency></qw-currency>
+  <hr>
+  <h4>Show the currency component changing the prices</h4>
+  <qw-room-rates
+    qw-room-rates-room-id="98868"
+    qw-room-rates-force-rooms-call="true">
+  </qw-room-rates>
+`;
--- src/components/qw-language/qw-language.stories.ts
+++ src/components/qw-language/qw-language.stories.ts
@@ -4,6 +4,12 @@ export default {
 
 export const base = () => `
   <qw-language></qw-language>
+  <hr>
+  <h4>Show the language component changing the language</h4>
+  <qw-room-rates
+    qw-room-rates-room-id="98868"
+    qw-room-rates-force-rooms-call="true">
+  </qw-room-rates>
 `;
 
 export const filterLanguages = () => `
