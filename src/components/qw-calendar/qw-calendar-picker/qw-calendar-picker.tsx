61db3fdca78db1d0290640c3886e71e0b27ec300
Matteo Barone <barmat90@gmail.com>
2020-03-25 17:45:48 +0100

    refactor(calendar): split code

--- src/components/qw-calendar/qw-calendar-picker/qw-calendar-picker.tsx
+++ src/components/qw-calendar/qw-calendar-picker/qw-calendar-picker.tsx
@@ -12,7 +12,7 @@ const CALENDAR_ID = 'flatpickr-element';
 @Component({
   tag: 'qw-calendar-picker',
   styleUrl: 'qw-calendar-picker.css',
-  shadow: false
+  shadow: false,
 })
 export class QwCalendarPicker {
   @Prop() qwCalendarPickerDisabled: boolean;
@@ -27,7 +27,7 @@ export class QwCalendarPicker {
   @Event() qwCalendarPickerChangeDates: EventEmitter<SessionStayPeriod>;
 
   private elementCalendarInstance: HTMLElement;
-  private configCalendarInstance: { [key: string]: any } = {};
+  private configCalendarInstance: {[key: string]: any} = {};
 
   public componentDidLoad() {
     this.initCalendar();
@@ -58,20 +58,24 @@ export class QwCalendarPicker {
       const datesToEmit = dateStr.split(' to ');
       const newStayPeriod: SessionStayPeriod = {
         arrivalDate: datesToEmit[0],
-        departureDate: datesToEmit[1] || datesToEmit[0]
+        departureDate: datesToEmit[1] || datesToEmit[0],
       };
       this.qwCalendarPickerChangeDates.emit(newStayPeriod);
     }
   };
 
-  private updateConfigCalendar(options) {
+  private updateConfigCalendarAndRefresh(options) {
     this.configCalendarInstance = {
       ...this.configCalendarInstance,
       ...options,
     };
 
     // todo: capire perché non funziona il set, ma solo reistanziandolo
-    this.calendarInstance = flatpickr(this.elementCalendarInstance, this.configCalendarInstance);
+    this.calendarInstance = this.getInstance();
+  }
+
+  private getInstance() {
+    return flatpickr(this.elementCalendarInstance, this.configCalendarInstance);
   }
 
   private setOneOrTwoMonthIfResponsive() {
@@ -82,12 +86,12 @@ export class QwCalendarPicker {
     const width = window.innerWidth;
     if (width > this.qwCalendarPickerDesktopLimit && this.qwCalendarPickerNumberOfMonths === 1) {
       this.qwCalendarPickerNumberOfMonths = 2;
-      this.updateConfigCalendar({showMonths: this.qwCalendarPickerNumberOfMonths});
+      this.updateConfigCalendarAndRefresh({showMonths: this.qwCalendarPickerNumberOfMonths});
     }
 
     if (width <= this.qwCalendarPickerDesktopLimit && this.qwCalendarPickerNumberOfMonths === 2) {
       this.qwCalendarPickerNumberOfMonths = 1;
-      this.updateConfigCalendar({showMonths: this.qwCalendarPickerNumberOfMonths});
+      this.updateConfigCalendarAndRefresh({showMonths: this.qwCalendarPickerNumberOfMonths});
     }
   }
 
@@ -108,16 +112,15 @@ export class QwCalendarPicker {
 
   @Watch('qwCalendarPickerStayPeriod')
   watchStayPeriod(newValue: SessionStayPeriod) {
-    this.updateConfigCalendar({defaultDate: [newValue.arrivalDate, newValue.departureDate]});
+    this.updateConfigCalendarAndRefresh({defaultDate: [newValue.arrivalDate, newValue.departureDate]});
   }
 
   @Watch('qwCalendarPickerLocale')
   watchLocale() {
-    this.updateConfigCalendar({locale: this.getLocaleForCalendar()});
+    this.updateConfigCalendarAndRefresh({locale: this.getLocaleForCalendar()});
   }
 
-
-  @Listen('resize', { target: 'window' })
+  @Listen('resize', {target: 'window'})
   handleScroll() {
     this.setOneOrTwoMonthIfResponsive();
   }
