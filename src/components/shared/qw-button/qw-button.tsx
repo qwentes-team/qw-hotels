8f5e5cf623cd7670bf0dcfc0762ac2ad321bbe22
roberta <roberta.gardiman@qwentes.it>
2020-12-17 17:45:37 +0100

    fix(console): removed qwButton icon error if icon name is not set

--- src/components/shared/qw-button/qw-button.tsx
+++ src/components/shared/qw-button/qw-button.tsx
@@ -18,7 +18,7 @@ export const QwButton: FunctionalComponent<QwButtonProps> = (props) => {
         ${props.QwButtonIcon ? 'qw-button--icon' : ''}
         ${props.QwButtonClass || ''}
       `}
-      style={{backgroundImage: "url(" + `https://hotels-booking-engine.qwentes.it/versions/v1/img/icons/${props.QwButtonIconFileName}` + ")"}}
+      style={props.QwButtonIconFileName && {backgroundImage: "url(" + `https://hotels-booking-engine.qwentes.it/versions/v1/img/icons/${props.QwButtonIconFileName}` + ")"}}
       onClick={() => props.QwButtonOnClick && props.QwButtonOnClick()}>
       {props.QwButtonLabel}
     </div>
