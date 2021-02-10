53de9965ddfbe6657eb6e9ba1c7ce9b525952177
roberta <roberta.gardiman@qwentes.it>
2021-02-10 11:23:09 +0100

    feat(rate): extra summary hidden if not present + changed label from guests to numberofguests

--- src/components/qw-extra/qw-extra-card/qw-extra-card.tsx
+++ src/components/qw-extra/qw-extra-card/qw-extra-card.tsx
@@ -86,7 +86,7 @@ export class QwExtraCard {
         </div>
         <div class="qw-extra-card__title">
           <h4>{this.qwExtraCardName}</h4>
-          <div class="qw-extra-card__summary">
+          {this.qwExtraCardSummary && <div class="qw-extra-card__summary">
             <div
               class="qw-extra-card__summary-trigger"
               onClick={() => this.qwExtraShowSummary = !this.qwExtraShowSummary}>
@@ -95,7 +95,7 @@ export class QwExtraCard {
             {this.qwExtraShowSummary && <div class="qw-extra-card__summary-content">
               {this.qwExtraCardSummary}
             </div>}
-          </div>
+          </div>}
         </div>
         <div class="qw-extra-card__footer">
           <div class="qw-extra-card__price">
