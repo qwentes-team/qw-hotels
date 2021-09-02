e644d03abdcaee5c256ec674a7ee668cee6f6a01
Luca Bellomo <luca.bellomo@qwentes.it>
2021-09-02 09:23:45 +0200

    update booking-state-manager

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,18 +2,7 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'qw-hotels',
-  commonjs: {
-    namedExports: {
-      '@qwentes/booking-state-manager': [
-        'SessionService', 'SessionHelper', 'SessionLoaded$', 'SessionIsLoading$', 'SessionHasRooms$', 'SessionHasRoomsSync',
-        'RoomService', 'RoomHelper', 'RoomLoaded$', 'RoomIsLoading$', 'RoomDefaultLabel', 'RoomSummaryType', 'RoomAvailability',
-        'BasketService', 'BasketHelper', 'BasketWithPrice$', 'BasketIsLoading$',
-        'DateUtil', 'DateFormat', 'MONEY_SYMBOLS', 'MONEY_CURRENCIES', 'RateHelper', 'createRateFromRoomBasketOccupancy',
-        'ExtraService', 'ExtraLoaded$', 'ExtraHelper', 'ExtraIsLoading$',
-        'QuoteService', 'QuoteHelper', 'QuoteLoaded$', 'RateQualifierType', 'Language', 'QwLanguageKeys',
-      ]
-    }
-  },
+  commonjs: {},
   hashFileNames: true,
   outputTargets: [
     {
